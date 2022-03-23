b379ac5f0a1ccf00adcad47edd84221f617520fe
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2022-03-23 10:29:03 +0800

    chore: delete useless codes

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 import { resolve } from 'path';
-import vueJsx from '@vitejs/plugin-vue-jsx'
 
 function pathResolve(dir: string) {
     return resolve(process.cwd(), '.', dir);
@@ -10,7 +9,7 @@ function pathResolve(dir: string) {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), vueJsx(), viteCommonjs(), esbuildCommonjs()],
+    plugins: [vue(), viteCommonjs(), esbuildCommonjs()],
     css: {
         preprocessorOptions: {
             less: {
